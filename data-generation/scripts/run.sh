2f20fc0b26104a61150a8288366d03f4c8297423
edixiong <xiongzheyang@gmail.com>
2024-10-14 04:48:30 +0000

    updates

--- data-generation/scripts/run.sh
+++ data-generation/scripts/run.sh
@@ -1,6 +1,6 @@
 python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_ntemplate_ordered_5_3.yaml --seed=0
 python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_wtemplate_ordered_5_3.yaml --seed=0
-# python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_ntemplate_ordered_5_3.yaml --seed=1
-# python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_wtemplate_ordered_5_3.yaml --seed=1
-# python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_ntemplate_ordered_5_3.yaml --seed=2
-# python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_wtemplate_ordered_5_3.yaml --seed=2
+python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_ntemplate_ordered_5_3.yaml --seed=1
+python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_wtemplate_ordered_5_3.yaml --seed=1
+python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_ntemplate_ordered_5_3.yaml --seed=2
+python generate_dataset.py --yaml-path ../dataset/yaml/simpledict_ndicts85_34_wtemplate_ordered_5_3.yaml --seed=2
